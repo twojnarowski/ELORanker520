3e38fe34c623613b9946e2a8453ee554b9a9243e
twojnarowski <tym17@24x7.pl>
2022-11-18 21:09:35 +0100

    Added models

---
+++ ELORanker520/Models/Match.cs
@@ -0,0 +1,8 @@
+﻿namespace ELORanker520.Models;
+
+public class Match
+{
+    public Player Player1 { get; set; }
+    public Player Player2 { get; set; }
+    public bool DidPlayer1Win { get; set; }
+}
---
+++ ELORanker520/Models/Player.cs
@@ -0,0 +1,8 @@
+﻿namespace ELORanker520.Models;
+
+public class Player
+{
+    public int Id { get; set; }
+
+    public int Score { get; set; }
+}
